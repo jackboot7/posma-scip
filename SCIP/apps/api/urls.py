4685c0ced11511adce4e86e8ff7ec35002d2d0f1
cbruguera <cbruguera@gmail.com>
2014-11-24 12:42:48 -0430

    agregado archivo de urls: api.urls

---
+++ SCIP/apps/api/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import patterns, include, url
+
+from apps.api.views import *
+
+
+users_urls = patterns(
+    '',
+    url(r'^/$', UsersView.as_view()),
+
+    url(r'^/(?P<username>[0-9a-zA-Z_-]+)/workdays/last(/)?$', UserLastWorkdayView.as_view()),
+    url(r'^/(?P<username>[0-9a-zA-Z_-]+)/workdays(/)?$', UserWorkdaysView.as_view()),
+    url(r'^/(?P<username>[0-9a-zA-Z_-]+)(/)?$', SpecificUserView.as_view()),
+
+)
+
+workdays_urls = patterns(
+    '',
+    url(r'^(/)?$', WorkdaysView.as_view()),
+    url(r'^/(?P<pk>[0-9]+)(/)?$', SpecificWorkdayView.as_view()),
+)
+
+urlpatterns = patterns(
+    '',
+    url(r'^users', include(users_urls)),
+    url(r'^workdays', include(workdays_urls))
+)
