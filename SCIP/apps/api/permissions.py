44319a68b9e3c72be2ff58bea9d60bb014f0f091
cbruguera <cbruguera@gmail.com>
2014-11-25 18:12:11 -0430

    corrección menor en comentarios de permissions.py

--- SCIP/apps/api/permissions.py
+++ SCIP/apps/api/permissions.py
@@ -3,9 +3,9 @@ from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     """
-    Custom permission to only allow owners of an object to edit or delete it.
+    Custom permission to only allow the corresponding user to handle data related to himself.
     """
 
     def has_object_permission(self, request, view, obj=None):
-        # Write permissions are only allowed to the owner of the snippet
+        # obj must be an instance of django.contrib.auth.models.User
         return obj is None or obj == request.user or request.user.is_staff
