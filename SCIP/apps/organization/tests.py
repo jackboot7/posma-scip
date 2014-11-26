58b782ad422510f8c15ab0e0654dc314c463abc5
cbruguera <cbruguera@gmail.com>
2014-11-26 15:43:22 -0430

    agregados métodos de testing para el modelo de organización (User y Workday)

--- SCIP/apps/organization/tests.py
+++ SCIP/apps/organization/tests.py
@@ -8,13 +8,22 @@ from django.core.exceptions import ValidationError
 from apps.organization.models import *
 
 
-class ProfileTestCase(TestCase):
+class UserTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create(username="burrg", email="burrg@harrb.gov", password="posma")
 
     def test_profile_exists(self):
         profile = Profile.objects.get(user=self.user)
+
         self.assertIsNotNone(profile)
+
+    def test_delete_user(self):
+        profile = Profile.objects.get(user=self.user)
+        profile_id = profile.id
+        self.user.delete()
+        profile = Profile.objects.filter(id=profile_id)
+
+        self.assertFalse(profile)
 
 
 class WorkdayTestCase(TestCase):
@@ -25,6 +34,7 @@ class WorkdayTestCase(TestCase):
         utcnow = datetime.utcnow()
         utcnow = utcnow.replace(tzinfo=pytz.utc)
         work = Workday.objects.create(user=self.user, start=utcnow)
+
         self.assertIsNotNone(work)
 
     def test_backward_datetimes(self):
@@ -32,6 +42,7 @@ class WorkdayTestCase(TestCase):
                                       start=datetime(2013, 11, 20, 20, 8, 7, 127325, tzinfo=pytz.UTC))
         work.finish = datetime(2013, 11, 20, 15, 8, 7, 127325, tzinfo=pytz.UTC)
         # finish time is earlier than start time
+
         self.assertRaises(ValidationError, work.clean)
 
     def test_overlapping_datetimes(self):
@@ -42,5 +53,14 @@ class WorkdayTestCase(TestCase):
         work2 = Workday.objects.create(user=self.user,
                                        start=datetime(2013, 11, 20, 16, 8, 7, 127325, tzinfo=pytz.UTC),
                                        finish=datetime(2013, 11, 20, 23, 8, 7, 127325, tzinfo=pytz.UTC))
+
+        self.assertRaises(ValidationError, work2.clean)
+
+    def test_unclosed_workday(self):
+        work1 = Workday.objects.create(user=self.user,
+                                       start=datetime(2013, 11, 20, 9, 8, 7, 127325, tzinfo=pytz.UTC))
+        work1.save()
+        work2 = Workday.objects.create(user=self.user,
+                                       start=datetime(2013, 11, 20, 10, 8, 7, 127325, tzinfo=pytz.UTC))
 
         self.assertRaises(ValidationError, work2.clean)
