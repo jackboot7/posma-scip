72b5d8842c0e28f151021b5a378370c7211749b8
cbruguera <cbruguera@gmail.com>
2014-12-08 17:11:54 -0430

    [frontend] UserListController redirecciona a 404 si el usuario no es staff

--- SCIP/static/js/controllers.js
+++ SCIP/static/js/controllers.js
@@ -22,10 +22,13 @@ scipControllers.controller('LoginController', ['$scope', '$window', '$rootScope'
                 {username:$scope.username, password:$scope.password},
 
                 function(data){
+                    var user_obj = jwtHelper.decodeToken(data.token)
+
                     $window.sessionStorage.token = data.token;
-                    $window.sessionStorage.user = JSON.stringify(jwtHelper.decodeToken(data.token));
+                    $window.sessionStorage.user = JSON.stringify(user_obj);
 
                     $rootScope.logged = true;
+                    $rootScope.is_staff = user_obj.is_staff;
                     $location.path('/');
 
                 }, 
@@ -35,6 +38,7 @@ scipControllers.controller('LoginController', ['$scope', '$window', '$rootScope'
                     delete $window.sessionStorage.token;
                     delete $window.sessionStorage.user;
                     $rootScope.logged = false;
+                    delete $rootScope.is_staff;
                     // problema con la conexión con el API. 
                     // Mostrar mensaje de error y redireccionar.
                 });
@@ -62,6 +66,10 @@ scipControllers.controller('CheckinController',['$scope', '$rootScope', '$locati
 scipControllers.controller('UserListController', ['$scope', '$rootScope', '$location', 'Users', function($scope, $rootScope, $location, Users){
     console.log($rootScope.logged);
     if ($rootScope.logged){
+        if (!$rootScope.is_staff) {
+            console.log("el usuario no es admin");
+            $location.path('/404');
+        }
         // La conexión con el API se hace sólo para usuarios loggeados.
         Users.get(
                 function(data){
