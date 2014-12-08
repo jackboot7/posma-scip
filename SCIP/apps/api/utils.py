3af2206763831ab3e0008dee38d9d6fdc765f846
cbruguera <cbruguera@gmail.com>
2014-12-08 16:12:08 -0430

    se quitaron los campos is_working, last_workday y average_hours del Token Payload del usuario

--- SCIP/apps/api/utils.py
+++ SCIP/apps/api/utils.py
@@ -10,5 +10,8 @@ def jwt_payload_handler(user):
     Function that builds a user-info object when encoding the tokens for JWT authentication
     """
     serializer = UserSerializer(user)
+    serializer.fields.pop('last_workday')
+    serializer.fields.pop('is_working')
+    serializer.fields.pop('average_hours_worked')
     data = serializer.data
     return data
