be49b106b70f313a3815e2c90cd70d007e54448b
Luis Alberto Santana <jackboot7@gmail.com>
2016-03-22 14:40:23 -0430

    Current date starts at 00 of the day.

--- SCIP/static/js/userlist.controller.js
+++ SCIP/static/js/userlist.controller.js
@@ -30,7 +30,7 @@
         $scope.getClass = function(user){
             var current_date = new Date();
             var date = new Date(user.last_workday.start);
-            if (date.toDateString() < current_date.toDateString()){
+            if (date < current_date.setHours(0,0,0,0)){
                 return { nochecked: true };
             }
         }
